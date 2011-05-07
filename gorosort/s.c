9a744efc3ffe24e82a591ba0d70ee7a743b529bc
Juarez Bochi <jbochi@gmail.com>
2011-05-07 18:16:29 -0300

    Solved GoroSort

--- gorosort/s.c
+++ gorosort/s.c
@@ -12,12 +12,12 @@ int main() {
 		int arr[n];
 
 		int i;
-		printf("INPUT: ");
+		//printf("INPUT: ");
 		for (i = 0; i < n; i++) {
 			scanf("%d", &arr[i]);
-			printf("%d ", arr[i]);
+			//printf("%d ", arr[i]);
 		}
-		printf("\n");
+		//printf("\n");
 
 		int swaps = 0;
 		int done;
@@ -25,18 +25,20 @@ int main() {
 			done = -1;
 			for (i = 0; i < n; i++) {
 				if (arr[i] != i + 1) {
+					/*
 					int temp;
 					temp = arr[arr[i] - 1];
-					printf("subs %d %d\n", arr[i], arr[arr[i] - 1]);
+					//printf("subs %d %d\n", arr[i], arr[arr[i] - 1]);
 					arr[arr[i] - 1] = arr[i];
 					arr[i] = temp;
-					swaps++;
 					done = 0;
+					*/
+					swaps++;
 				}
 			}
 		} while (!done);
 
-		printf("Case #%d: %d.000000\n", test, swaps * 2);
+		printf("Case #%d: %d.000000\n", test, swaps);
 	}
 	return 0;
 }
